9c9acbd35dc9a2c3d9cee33463eff95f7d1d40a2
vineel <vineelgolla@gmail.com>
2019-12-31 12:52:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/heartrate/ForgotPassword.java
@@ -0,0 +1,16 @@
+package com.example.heartrate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*Created by
+    NAGA VINEEL GOLLA
+*/
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
